ab33644a067bfcfdf3ef288d8116591ad18d5cfb
manumafe98 <manumaxerapro@gmail.com>
2024-12-04 23:05:11 -0300

    Start working with JSOUP to parse the transfermarket chelsea current detailed stats

---
+++ playersdata/src/test/java/com/manumafe/players/data/playersdata/PlayersdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manumafe.players.data.playersdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayersdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

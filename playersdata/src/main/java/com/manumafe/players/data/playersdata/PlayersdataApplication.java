10062417a920a2b97730fea1fa232958aab97fef
manumafe98 <manumaxerapro@gmail.com>
2024-12-09 21:56:49 -0300

    Separate scrape, scheduling and save into 3 different player services

--- playersdata/src/main/java/com/manumafe/players/data/playersdata/PlayersdataApplication.java
+++ playersdata/src/main/java/com/manumafe/players/data/playersdata/PlayersdataApplication.java
@@ -2,8 +2,10 @@ package com.manumafe.players.data.playersdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PlayersdataApplication {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(PlayersdataApplication.class, args);

3eafa6bdd56d5e0a7de9718c750dc4e1fa35071d
manumafe98 <manumaxerapro@gmail.com>
2024-12-11 20:45:11 -0300

    Start structuring the frontend, and adding necessary dependencies (tailwind)

---
+++ frontend/src/ChelseaTriviaApp.tsx
@@ -0,0 +1,7 @@
+
+export const ChelseaTriviaApp = () => {
+  return (
+    <>
+    </>
+  )
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ChelseaTriviaApp } from './ChelseaTriviaApp'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ChelseaTriviaApp />
   </StrictMode>,
 )
